e02ed9f36deb09f78aea62855773e35144d6d175
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-06-20 13:57:49 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/paytm")
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, lowercase: true, trim: true, minLength: 3, maxLength: 30 },
+    password: { type: String, required: true, minLength: 6 },
+    firstName: { type: String, required: true, trim: true, maxLength: 50 },
+    lastName: { type: String, required: true, trim: true, maxLength: 50 }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+}
